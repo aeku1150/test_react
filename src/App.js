e90f782e23d4e5cbb2cb2c0576b205ba3bf0c0b7
unknown <tf4hyute@gmail.com>
2025-01-30 14:33:13 +0700

    index

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './Navbar'; // นำเข้า Navbar
 
 function App() {
   return (
     <div className="App">
+      <Navbar /> {/* เพิ่ม Navbar */}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
